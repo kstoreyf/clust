5b803c7ecf5d89d627c28a489ad95f06b6a52444
Kate Storey-Fisher <kstoreyfisher@gmail.com>
2021-02-28 20:11:37 -0500

    glam statistics

---
+++ calc_mean_numgals_glam.py
@@ -0,0 +1,25 @@
+import numpy as np
+import subprocess
+
+
+mock_dir = '/mount/sirocco2/zz681/emulator/GLAM_mock/mocks2'
+tag = '_glam'
+
+nmocks = 986
+ngals_arr = []
+for nmock in range(nmocks):
+  fn = f'{mock_dir}/mock_n{nmock}_test_0.mock'
+  p = subprocess.Popen(['wc', '-l', fn], stdout=subprocess.PIPE, 
+                                                  stderr=subprocess.PIPE)
+  result, err = p.communicate()
+  if p.returncode != 0:
+    raise IOError(err)
+  ngals = int(result.strip().split()[0])
+  ngals_arr.append(ngals)
+
+print(np.mean(ngals_arr))
+print(np.std(ngals_arr))
+print(np.max(ngals_arr))
+print(np.min(ngals_arr))
+
+np.savetxt("numgals/mean_numgals{}.dat".format(tag), [np.mean(ngals_arr)])
